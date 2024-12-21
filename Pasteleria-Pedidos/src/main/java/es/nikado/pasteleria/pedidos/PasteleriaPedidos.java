e9b65fbb58a8a1259097de26dfa2a93d409a3f30
AdolfoJF <adolfo.jf.93@gmail.com>
2024-12-21 20:05:26 +0100

    Primer commit con estructura básica

---
+++ Pasteleria-Pedidos/src/main/java/es/nikado/pasteleria/pedidos/PasteleriaPedidos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.nikado.pasteleria.pedidos;
+
+/**
+ *
+ * @author AdolfoJF
+ */
+public class PasteleriaPedidos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
